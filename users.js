de67107bbe08e8cd133562135136f29df94b21ca
Jeanius123 <chanyut22@hotmail.com>
2020-01-28 16:38:43 +0700

    second commit

--- users.js
+++ users.js
@@ -1,20 +1,37 @@
 // Using https://www.youtube.com/watch?v=-NsHWrrUgdk as guide
 
+const express = require("express");
+const router = express.Router();
+
 var admin = require("firebase-admin");
 
-var serviceAccount = require("path/to/serviceAccountKey.json");
+var serviceAccount = require("./networking-bangkok-js-firebase-adminsdk-d3d5j-1b2eb3ffbf.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://networking-bangkok-js.firebaseio.com"
 });
 
-// // Firebase App (the core Firebase SDK) is always required and
-// // must be listed before other Firebase SDKs
-// const firebase = require("firebase/app");
-
-// // Add the Firebase products that you want to use
-// require("firebase/auth");
-// require("firebase/firestore");
-
 database = admin.firestore();
+collection = database.collection("users");
+console.log(collection);
+
+router.get("/users", (req, res, next) => {
+    let allUsers = [];
+    collection.get().then(
+        snapshot => {
+            snapshot.forEach(doc => {
+                allUsers.push({
+                    "id": "000",
+                    "networks": {
+                        0: "0",
+                        1: "2",
+                        2: "4",
+                    }
+                })
+            })
+        }
+    )
+});
+
+module.exports = router;
