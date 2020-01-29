8aafc7922a495becfb60328356ce62b51c4c6e95
Jeanius123 <chanyut22@hotmail.com>
2020-01-29 16:08:34 +0700

    Deploy complete

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+
+// Add middleware to authenticate requests
+const firestoreAPI = require("./app.js"); 
+app.use("/", firestoreAPI);
+
+// Expose Express API as a single Cloud Function:
+exports.widgets = functions.https.onRequest(app);
